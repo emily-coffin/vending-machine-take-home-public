d885aa35ac9ab62b99256b0e58679b69dd1de6eb
Emily Coffin <emily.coffin@homeinsteadinc.com>
2021-08-21 15:52:24 -0500

    Updates method to return coin instead of value

--- UnitTests/MachineTests.cs
+++ UnitTests/MachineTests.cs
@@ -44,18 +44,17 @@ namespace UnitTests
         }
 
         [Theory]
-        [InlineData(0.01, 2.5, 0.75, 1.52)]
-        [InlineData(0.05, 5, 0.835, 1.95)]
-        [InlineData(0.10, 2.268, 0.705, 1.35)]
-        [InlineData(0.25, 5.67, 0.955, 1.75)]
-        [InlineData(.50, 11.34, 1.205, 2.15)]
-        [InlineData(1.00, 8.1, 1.043, 2.00)]
-        public void FindCoinValueIsReturnedCorrectly(double expectedValue, double weight, double diameter, double thinkness)
+        [InlineData("Penny", 0.01, 2.5, 0.75, 1.52)]
+        [InlineData("Nickle", 0.05, 5, 0.835, 1.95)]
+        [InlineData("Dime", 0.10, 2.268, 0.705, 1.35)]
+        [InlineData("Quarter", 0.25, 5.67, 0.955, 1.75)]
+        public void FindCoinIsReturnsCorrectCoinWhenGivenCoinProperties(string expectedName, double expectedValue, double weight, double diameter, double thinkness)
         {
             var machine = new Machine(null);
-            var value = machine.FindCoinValue(weight, diameter, thinkness);
+            var coin = machine.FindCoin(weight, diameter, thinkness);
 
-            value.Should().Be(expectedValue);
+            coin.Name.Should().Be(expectedName);
+            coin.Value.Should().Be(expectedValue);
         }
 
         [Theory]
@@ -82,5 +81,19 @@ namespace UnitTests
 
             isAvailable.Should().Be(expectedAvailability);
         }
+
+        // [Theory]
+        // [InlineData("Penny", 0.01, 2.5, 0.75, 1.52)]
+        // [InlineData("Nickle", 0.05, 5, 0.835, 1.95)]
+        // [InlineData("Dime", 0.10, 2.268, 0.705, 1.35)]
+        // [InlineData("Quarter", 0.25, 5.67, 0.955, 1.75)]
+        // public void AddCoinsToPaymentAppendsCoinsEnteredByCustomer(double expectedValue, double weight, double diameter, double thinkness)
+        // {
+        //     var machine = new Machine(null);
+        //     var value = machine.AddCoinsToPayment(weight, diameter, thinkness);
+
+        //     var expectedCoinList =
+        //     return
+        // }
     }
 }
--- VendingMachine/Machine.cs
+++ VendingMachine/Machine.cs
@@ -15,28 +15,23 @@ namespace VendingMachine
 
         public Product BuyProduct(string productName)
         {
-
-
             return products
                    .Where(product => product.Name == productName)
                    .FirstOrDefault();
         }
 
-        public double FindCoinValue(double weight, double diameter, double thinkness)
+        public Coin FindCoin(double weight, double diameter, double thinkness)
         {
             List<Coin> coins = new List<Coin>()
             {
                 new Coin() { Name = "Penny", Value = 0.01, Weight = 2.5, Diameter = 0.75, Thinkness = 1.52 },
                 new Coin() { Name = "Nickle", Value = 0.05, Weight = 5, Diameter = 0.835, Thinkness = 1.95 },
                 new Coin() { Name = "Dime", Value = 0.10, Weight = 2.268, Diameter = 0.705, Thinkness = 1.35 },
-                new Coin() { Name = "Quarter", Value = 0.25, Weight = 5.67, Diameter = 0.955, Thinkness = 1.75 },
-                new Coin() { Name = "Half Dollar", Value = 0.50, Weight = 11.34, Diameter = 1.205, Thinkness = 2.15 },
-                new Coin() { Name = "Dollar Coin", Value = 1.00, Weight = 8.1, Diameter = 1.043, Thinkness = 2.00 }
+                new Coin() { Name = "Quarter", Value = 0.25, Weight = 5.67, Diameter = 0.955, Thinkness = 1.75 }
             };
 
             return coins
                    .Where(coin => coin.Weight == weight && coin.Diameter == diameter && coin.Thinkness == thinkness)
-                   .Select(coin => coin.Value)
                    .FirstOrDefault();
         }
 
