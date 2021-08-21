a456aec9c292d53c41ae0d3581c87f57acaad73b
Emily Coffin <emily.coffin@homeinsteadinc.com>
2021-08-21 15:16:40 -0500

    Adds a test to find coin value

--- UnitTests/MachineTests.cs
+++ UnitTests/MachineTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using VendingMachine;
 using VendingMachine.Models;
@@ -29,6 +30,21 @@ namespace UnitTests
             var product = machine.BuyProduct(productName);
 
             product.Should().BeEquivalentTo(expectedProduct);
+        }
+
+        [Theory]
+        [InlineData( 0.01, 2.5, 0.75, 1.52)]
+        [InlineData(0.05, 5, 0.835, 1.95)]
+        [InlineData(0.10, 2.268, 0.705, 1.35)]
+        [InlineData(0.25, 5.67, 0.955, 1.75)]
+        [InlineData(.50, 11.34, 1.205, 2.15)]
+        [InlineData(1.00, 8.1, 1.043, 2.00)]
+        public void FindCoinValueIsReturnedCorrectly(double expectedValue, double weight, double diameter, double thinkness)
+        {
+            var machine = new Machine();
+            var value = machine.FindCoinValue(weight, diameter, thinkness);
+
+            value.Value.Should().Be(value);
         }
     }
 }
