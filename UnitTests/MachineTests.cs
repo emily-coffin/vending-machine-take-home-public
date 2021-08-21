c709b19937c9dd9aca77bc8665192c90c213ec28
Emily Coffin <emily.coffin@homeinsteadinc.com>
2021-08-21 15:04:25 -0500

    Writes test for returning specific product

--- UnitTests/MachineTests.cs
+++ UnitTests/MachineTests.cs
@@ -12,9 +12,23 @@ namespace UnitTests
         public void BuyProductReturnsProductType()
         {
             var machine = new Machine();
-            var product = machine.BuyProduct();
+            var product = machine.BuyProduct(null);
 
             product.Should().BeOfType<Product>();
+        }
+
+        [Theory]
+        [InlineData("Cola")]
+        [InlineData("Chips")]
+        [InlineData("Candy")]
+        public void BuyProductReturnsProductWhenSelected(string productName)
+        {
+            var expectedProduct = new Product() { Name = productName };
+
+            var machine = new Machine();
+            var product = machine.BuyProduct(productName);
+
+            product.Should().BeEquivalentTo(expectedProduct);
         }
     }
 }
