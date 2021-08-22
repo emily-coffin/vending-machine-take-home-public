3af22a6a9e47e8adab631237a099c84f5a87e755
Emily Coffin <emily.coffin@homeinsteadinc.com>
2021-08-22 07:53:56 -0500

    Fixes test to check by coin properties instead of value

--- UnitTests/CoinHelperTests.cs
+++ UnitTests/CoinHelperTests.cs
@@ -33,17 +33,16 @@ namespace UnitTests
         }
 
         [Theory]
-        [InlineData( 0.01, 2.5, 0.75, 1.52)]
-        [InlineData(0.05, 5, 0.835, 1.95)]
-        [InlineData(0.10, 2.268, 0.705, 1.35)]
-        [InlineData(0.25, 5.67, 0.955, 1.75)]
-        [InlineData(.50, 11.34, 1.205, 2.15)]
-        [InlineData(1.00, 8.1, 1.043, 2.00)]
-        public void FindCoinByPropertiesReturnsCorrectCoin(double expectedValue, double weight, double diameter, double thinkness)
+        [InlineData("Penny", 0.01, 2.5, 0.75, 1.52)]
+        [InlineData("Nickel", 0.05, 5, 0.835, 1.95)]
+        [InlineData("Dime", 0.10, 2.268, 0.705, 1.35)]
+        [InlineData("Quarter", 0.25, 5.67, 0.955, 1.75)]
+        public void FindCoinByPropertiesReturnsCorrectCoin(string name, double value, double weight, double diameter, double thinkness)
         {
-            var value = CoinHelper.FindCoinByProperties(weight, diameter, thinkness);
+            var coin = CoinHelper.FindCoinByProperties(weight, diameter, thinkness);
 
-            value.Should().Be(expectedValue);
+            var expectedCoin = new Coin() { Name = name, Value = value, Weight = weight, Diameter = diameter, Thinkness = thinkness };
+            coin.Should().BeEquivalentTo(expectedCoin);
         }
     }
 }
