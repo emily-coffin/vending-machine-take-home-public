38136795ecaddc941cc60cf177f2c42fbbcdab96
Emily Coffin <emily.coffin@homeinsteadinc.com>
2021-08-22 10:45:18 -0500

    Adds dimes into change return

--- UnitTests/MachineTests.cs
+++ UnitTests/MachineTests.cs
@@ -161,6 +161,7 @@ namespace UnitTests
             var coinsPaid = Enumerable
                             .Repeat(new Coin() { Name = "Quarter", Value = 0.25, Weight = 5.67, Diameter = 0.955, Thinkness = 1.75 },6)
                             .ToList();
+            coinsPaid.Add(new Coin() { Name = "Dime", Value = 0.10, Weight = 2.268, Diameter = 0.705, Thinkness = 1.35 });
             coinsPaid.Add(new Coin() { Name = "Nickel", Value = 0.05, Weight = 5, Diameter = 0.835, Thinkness = 1.95 });
             coinsPaid.Add(new Coin() { Name = "Penny", Value = 0.01, Weight = 2.5, Diameter = 0.75, Thinkness = 1.52 });
 
@@ -171,6 +172,7 @@ namespace UnitTests
             {
                 new Coin() { Name = "Quarter", Value = 0.25, Weight = 5.67, Diameter = 0.955, Thinkness = 1.75 },
                 new Coin() { Name = "Quarter", Value = 0.25, Weight = 5.67, Diameter = 0.955, Thinkness = 1.75 },
+                new Coin() { Name = "Dime", Value = 0.10, Weight = 2.268, Diameter = 0.705, Thinkness = 1.35 },
                 new Coin() { Name = "Nickel", Value = 0.05, Weight = 5, Diameter = 0.835, Thinkness = 1.95 },
                 new Coin() { Name = "Penny", Value = 0.01, Weight = 2.5, Diameter = 0.75, Thinkness = 1.52 }
             };
